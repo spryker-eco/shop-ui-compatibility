860022351b225bd2bf05496124445d63a31cc72c
Ivan Pugach <ivan.pugach@spryker.com>
2018-10-03 23:39:08 +0300

    Improve module

--- src/SprykerEco/Yves/ShopUiCompatibility/Plugin/Provider/ShopUiCompatibilityTwigServiceProvider.php
+++ src/SprykerEco/Yves/ShopUiCompatibility/Plugin/Provider/ShopUiCompatibilityTwigServiceProvider.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
- * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
 namespace SprykerEco\Yves\ShopUiCompatibility\Plugin\Provider;
@@ -10,6 +10,7 @@ namespace SprykerEco\Yves\ShopUiCompatibility\Plugin\Provider;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 use Spryker\Yves\Kernel\AbstractPlugin;
+use Twig_Environment;
 
 /**
  * @method \SprykerEco\Yves\ShopUiCompatibility\ShopUiCompatibilityFactory getFactory()
@@ -26,7 +27,7 @@ class ShopUiCompatibilityTwigServiceProvider extends AbstractPlugin implements S
         $twigExtension = $this->getFactory()->createShopUiCompatibilityTwigExtension();
 
         $app['twig'] = $app->share(
-            $app->extend('twig', function (\Twig_Environment $twig) use ($twigExtension) {
+            $app->extend('twig', function (Twig_Environment $twig) use ($twigExtension) {
                 $twig->addExtension($twigExtension);
 
                 return $twig;
